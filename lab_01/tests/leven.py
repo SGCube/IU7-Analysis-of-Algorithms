ccd056b6b8c78f23ba66dd63af612cd77b8c8a6b
SGCube <sortech-sorgamer@yandex.ru>
2019-09-08 14:50:26 +0300

    #3 levenstein distance (calculation by matrix)

---
+++ lab_01/tests/leven.py
@@ -0,0 +1,32 @@
+# Levenshtein distance calculation functions
+
+
+def levenshtein_matrix(s1, s2):
+    s1_len = len(s1)
+    s2_len = len(s2)
+
+    # initialization of first two rows
+    prev_row = [i for i in range(s2_len + 1)]
+    print(prev_row)
+    current_row = [0] * (s2_len + 1)
+
+    for i in range(1, s1_len + 1):                      # row loop
+        # current row fill
+        current_row[0] = i
+        for j in range(1, s2_len + 1):                  # column loop
+            match_fault = int(s1[i - 1] != s2[j - 1])   # symbol match
+            current_row[j] = min(current_row[j - 1] + 1,        # horizontal
+                                 prev_row[j] + 1,               # vertical
+                                 prev_row[j - 1] + match_fault) # diagonal
+
+        print(current_row)
+        # row switching
+        prev_row = current_row.copy()
+        current_row.clear()
+        current_row = [0] * (s2_len + 1)
+
+    return prev_row[-1]
+
+
+if __name__ == "__main__":
+    print(levenshtein_matrix("error", "horror"))
