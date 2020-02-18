e44074627611b86bd8fe96f955b3984d96afa743
SGCube <sgcube@yandex.ru>
2020-02-18 10:45:34 +0300

    lab_06: source sode

---
+++ lab_06/bf.hpp
@@ -0,0 +1,25 @@
+#ifndef BF_HPP
+#define BF_HPP
+
+#include <vector>
+#include "graph.hpp"
+
+class BruteForce
+{
+private:
+    Graph<int> graph;
+    std::vector<bool> visited;
+    
+public:
+    size_t min_len = 0;
+    std::vector<size_t> min_path;
+    
+    BruteForce(const Graph<int>& graph);
+    void execute();
+    
+private:
+    void execute(const size_t vert, size_t cur_len,
+        std::vector<size_t>& cur_path);
+};
+
+#endif
